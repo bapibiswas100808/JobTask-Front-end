5a7cd2dd198e053ecd780d5d33fc3532c45258cd
bapibiswas100808 <star.bappy@gmail.com>
2024-08-15 11:21:59 +0600

    project started

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <>
+      <h2>This is Homepage</h2>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+import HomePage from "../Pages/HomePage/HomePage";
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [{ path: "/", element: <HomePage></HomePage> }],
+  },
+]);
+export default Router;
