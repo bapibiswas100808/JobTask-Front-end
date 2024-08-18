dcfb4e06d34cdb7339aa10609e06c977b0f64bc5
bapibiswas100808 <star.bappy@gmail.com>
2024-08-18 11:16:41 +0600

    project updated2

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,10 +4,12 @@ import HomePage from "../Pages/HomePage/HomePage";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
